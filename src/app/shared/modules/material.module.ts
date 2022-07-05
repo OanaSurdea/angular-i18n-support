58d402b0b40fdfe2d7f566d2c226987af04eecdc
RobotCherries <zdip14@gmail.com>
2022-07-05 19:04:06 +0300

    fix(ui-theme): rename the material folder to modules

---
+++ src/app/shared/modules/material.module.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatInputModule,
+    MatRadioModule,
+    MatMenuModule,
+    MatIconModule
+  ],
+  exports: [
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatInputModule,
+    MatRadioModule,
+    MatMenuModule,
+    MatIconModule
+  ]
+})
+export class MaterialUiModule { }
