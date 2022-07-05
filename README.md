563d5f1e9807bbae4e90ef84ab31d0999ccec5ad
RobotCherries <zdip14@gmail.com>
2022-07-05 11:26:37 +0300

    feat: add angular project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LearningInternationalization
+# LearnInternationalization
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.8.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/learning-internationalization'),
+      dir: require('path').join(__dirname, './coverage/learn-internationalization'),
       subdir: '.',
       reporters: [
         { type: 'html' },
