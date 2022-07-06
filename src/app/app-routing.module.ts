8c50999dc816076705110078b4569f3f1061be35
RobotCherries <zdip14@gmail.com>
2022-07-06 11:49:43 +0300

    feat(i18n): build config + routing updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
   { path: 'about', loadChildren: () => import('./features/about/about.module').then(m => m.AboutModule) }
 ];
 
