14594b623128a97404d40f1a2a16497c78a1654d
RobotCherries <zdip14@gmail.com>
2022-07-12 18:40:53 +0300

    BREAKING CHANGES: feat(setup): renamed project to angular-i18n-support

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/learn-internationalization'),
+      dir: require('path').join(__dirname, './coverage/angular-i18n-support'),
       subdir: '.',
       reporters: [
         { type: 'html' },
